16dbcf5434b180abd8f1a932f68e0e0c78aaa3b1
nmalif <nathan-malifarge@orange.fr>
2015-12-18 09:40:16 +0100

    commentaires intro

---
+++ gestionnaire.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# DEBUT INTRO
+
+# Si il y a un argument, on regarde si l'argument fini par .cpp.
+# Si c'est le  cas, MENU
+# Sinon, dire qu'il y a erreur et redemander un nom correcte de fichier .cpp
+
+# Sinon sil n'y en a pas, on fait un ls des .cpp du répertoire, si il y a des 
+# .cpp, on demander à lutilisateur decrire le nom du fichier.
+# Sinon (sil ny a pas de .cpp après le ls, on utilise le TEMPLATE)
+
+# FIN INTRO
+
+# DEBUT MENU
+
+# un truc genre --> voir : 0
+#                   éditer : 1
+#                   générer : 2
+#                   lancer : 3
+#                   débugguer :  4 ETC
+
+# On demande une réponse à l'utilisateur tant que sa réponse est incorrecte
+
+# FEATURES
+
+# voir : less
+# éditer : 
+
