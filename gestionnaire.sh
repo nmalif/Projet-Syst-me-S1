87e57c88fe7652b2c6da04994344743c31cc335e
VietKhangLH <vietkhangleho@gmail.com>
2015-12-24 18:41:18 +0100

    toute l'interface graphique de l'introduction est opérationnelle, il faut maintenant faire le choix des outils d'édition, via un menu

--- gestionnaire.sh
+++ gestionnaire.sh
@@ -7,7 +7,7 @@
 # pour des raisons de portabilité
 
 maketemplate()
-{ 
+{
     echo "#include <iostream>" > template.cpp
     echo "#include <cstdlib>" >> template.cpp
     echo " " >> template.cpp
@@ -25,12 +25,12 @@ listtomenu()
     if test tomenu
     then
         rm -f tomenu
-    fi 
+    fi
     local i=0
     while read fichier
     do
        let i++
-       echo -n \"$i\" >> tomenu
+       echo -n \"$i.\" >> tomenu
        echo -n " " >> tomenu
        printf "%s\n" "\"$fichier\" \\" >> tomenu
     done < temp
@@ -41,20 +41,36 @@ makemenuscript()
 {
     local nbfic
     let nbfic=$(wc -l tomenu | cut -d' ' -f 1)
-    
+
     cat $0 | head -n 1 > menu.sh
     chmod 700 menu.sh
-    echo 'fictoedit=$(whiptail --title "Voici les fichiers sources c++ du répertoire" --menu "Chosissez un fichier"' 0 100 $nbfic "\\" >> menu.sh
+    echo 'fictoedit=$(whiptail --title "Voici les fichiers sources c++ du répertoire" --menu "Chosissez un fichier"' 0 0 $nbfic "\\" >> menu.sh
     cat tomenu >> menu.sh
     echo "if [ \$? = 0 ]" >> menu.sh
     echo "then" >> menu.sh
     echo "    echo \$fictoedit" >> menu.sh
     echo "else" >> menu.sh
     echo "    echo \"Cancel\"" >> menu.sh
-    echo "fi" >> menu.sh 
+    echo "fi" >> menu.sh
 }
 
-# DEBUT INTRO
+msgbox()
+{
+    whiptail --title "$1" --msgbox "$2" 0 0
+}
+
+inputbox()
+{
+    nom=$(whiptail --title "$1" --inputbox "$2" 0 0 template 3>&1 1>&2 2>&3)
+    if [ $? = 0 ]
+    then
+        mv template.cpp $nom.cpp
+        echo $nom >> temp
+    else
+        echo "cancel"
+    fi
+}
+
 
 # VARIABLES
 fictoedit=""
@@ -64,57 +80,52 @@ var=$(cat tomenu)
 # FIN VARIABLES
 
 
-whiptail --title "Bienvenue !" --msgbox "Ceci est un outil d'aide à la programmation en C++, développé par Nathan Malifarge et Viet-Khang Le Ho, pour le projet Système S1." 8 100
-
 
 clear
+
+# DEBUT INTRO
+
+msgbox "Bienvenue !" "Ceci est un outil d'aide à la programmation en C++, développé par Nathan Malifarge et Viet-Khang Le Ho, pour le projet Système S1."
+
 ls -l | egrep \(*.cpp$\|*.cc$\) | grep -o "[^ ]*$" | cut -d'.' -f 1 > temp # tous les fichiers sources c++ dans un fichier, ligne par ligne, sans extension
 
 if [ $# -eq 0 ] # Si il ny pas dargument
 then # alors on met le nom de tous les fichiers .cc et .cpp dans un fichier temp
    if [ $(wc -l temp | cut -d' ' -f 1) -eq 0 ] # sil ny a pas de .cpp ou .cc dans le repertoire alors on utilise le TEMPLATE
    then
-      echo "Il n'y a pas de .cpp..."
-      echo "... un template a été automatiquement crée !"
+      msgbox "Il n'y a pas de .cpp dans le répertoire courant" "... un template a été automatiquement crée !"
       maketemplate
-      while $continuer
-          do
-              echo -n "Donnez lui un nom : "
-              read fictoedit
-              echo "Êtes-vous sûr ?"
-              read reponse
-              if [ $reponse = 'oui' ] || [ $reponse = 'Oui' ]
-              then
-                  continuer=false
-              fi
-          done 
-      continuer=true
-      mv template.cpp $fictoedit.cpp
-      echo $fictoedit > temp
+      inputbox "Vous pouvez nommer le fichier" "Quel nom voulez-vous lui donner ?"
    else # sinon on les montre
       listtomenu
       makemenuscript
       menu.sh
+      rm -f menu.sh tomenu
    fi
 elif [ $# -eq 1 ] # Sinon sil y a exactement un argument
 then
-   fictoedit=$1
-   echo -ne "Le fichier\033[31m" $fictoedit "\033[0m"
-   if  [ $(grep -cx $1 temp) = 1 ] # on regarde si le nom du fichier est présent dans temp, si oui
+   echo $1 > arg1
+   fictoedit=$(cat arg1 | cut -d'.' -f 1)
+   rm -f arg1
+
+   msgbox "Bienvenue !" "Vous avez choisi de manipuler le fichier $fictoedit."
+   if  [ $(grep -cx $fictoedit temp) = 1 ] # on regarde si le nom du fichier est présent dans temp, si oui
    then
-      echo "est présent"
+      msgbox "Informations" "Ce fichier est présent."
    else
-      echo "n'est pas présent"
-      touch $fictoedit.cpp
-      echo "Il a donc été crée !"
+      msgbox "Informations" "Ce fichier n'est pas présent, il a donc été crée !"
+      maketemplate
+      mv template.cpp $fictoedit.cpp
    fi
-   echo -e "Vous manipulez le fichier \033[31m$fictoedit\033[0m"
+   msgbox "Informations" "Vous manipulez le fichier $fictoedit."
 else # Sinon (dans ce cas là il y a plus d'un argument) il y a erreur car il y a plusieurs fichiers sélectionnés !
-   echo "il y a plus d'un argument, le script s'arrête"
+   msgbox "ERREUR FATALE" "Il y a plus d'un argument, le script va s'arrêter."
    exit 1
 fi
 
-#rm -f temp 
+rm -f temp
+
+
 # FIN INTRO
 
 # DEBUT MENU
